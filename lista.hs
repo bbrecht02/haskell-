0d7c8d4e1eb0a6ca22d4e27aee1fe36520475a81
Bennyson Brecht <49658184+bbrecht02@users.noreply.github.com>
2019-10-01 14:55:48 -0300

    Add files via upload

---
+++ lista.hs
@@ -0,0 +1,25 @@
+--lista 
+
+--lista de int 
+lista = [1,2,3,4,5]
+-- primeiro item da lista 
+head [1,2,3,4,5]
+
+--Out = 1
+
+-- corpo da lista 
+tail [1,2,3,4,5]
+
+--Out =  2,3,4,5
+
+head ["benny"]
+
+--Out = "benny"
+
+head "benny"
+
+--Out = 'b'
+
+tail "benny"
+
+--Out = "enny"
